3112664e7f3ae071db53e6764e7ccf52bc8b2b4e
Alexspace12 <ovidiu.petrisor@ucdconnect.ie>
2023-11-23 14:57:33 +0000

    Add practical 2

--- practical02/Sum.c
+++ practical02/Sum.c
@@ -8,23 +8,23 @@ int main(void) {
    
 
 /* First sum */
-   sum1 = 10.0;
+   sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      /*  Insert here */
+      sum1 = sum1 + 1./i;
    }
 
 
 /* Second sum */
-   sum2 = 20.0;
+   sum2 = 0.0;
    for (i=1000; i>0; i--) {
-      /* Insert the same line as above except use sum2 */
+      sum2 = sum2 + 1./i ;
    }
 
    printf(" Sum1=%f\n",sum1);
    printf(" Sum2=%f\n",sum2);
 
 /* Find the difference */
-   diff = /* ?? */
+   diff = sum1 - sum2;
 
    printf(" Difference between the two is %f\n",diff);
 
